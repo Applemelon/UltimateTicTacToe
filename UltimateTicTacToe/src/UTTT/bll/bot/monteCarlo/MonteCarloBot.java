36e27ef615aedffe6f158d6d0589ab3a1bef0912
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2018-03-05 09:56:50 +0100

    motecarlo start

---
+++ UltimateTicTacToe/src/UTTT/bll/bot/monteCarlo/MonteCarloBot.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UTTT.bll.bot.monteCarlo;
+
+import UTTT.bll.bot.IBot;
+import UTTT.bll.game.IGameState;
+import UTTT.bll.move.IMove;
+
+/**
+ *
+ * @author janvanzetten
+ */
+public class MonteCarloBot implements IBot{
+
+    @Override
+    public IMove doMove(IGameState state) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
